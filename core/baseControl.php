3feb27d055863f7357665b4d6f7a0ab8c9cce1e3
jim-lu <1069934205@qq.com>
2019-03-14 02:13:00 -0400

    Add files via upload

---
+++ core/baseControl.php
@@ -0,0 +1,40 @@
+<?php
+class baseControl{
+    private $smarty;
+
+    function __construct() {
+        $this->smarty = new Smarty();
+        $this->smarty->template_dir = "view";
+    }
+
+    public function model($model_name) {
+        require_once "model/$model_name.php";
+        return new $model_name("mysql:dbhost=localhost;dbname=project;charset=utf8","root","",true);
+    }
+    public function display($html) {
+        $this->smarty->display($html);
+    }
+
+    // 返回smarty编译好的html
+    public function fetch($html)
+    {
+        return $this->smarty->fetch($html);
+    }
+    public function assign($name, $value) {
+        $this->smarty->assign($name, $value);
+    }
+    public function run() {
+        $control = isset($_REQUEST['control']) ? $_REQUEST['control'] : "index";
+        $control_name = $control.'Control';
+        require_once "control/$control_name.php";
+        $sys = new $control_name();
+        if(!empty($_REQUEST['action'])) {
+            $action = $_REQUEST['action'];
+            $sys->$action();
+        }else {
+            $sys->index();
+        }
+    }
+}
+
+?>
